0e5bd4cf1cf39c3b17b54b7fe9c575aeea9c9fdd
metacodebean <caokun8008@gmail.com>
2022-11-10 20:06:45 +0800

    delete exe

--- src/ch02/demo01_变量声明.go
+++ src/ch02/demo01_变量声明.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var m = "19"
+
 func main() {
 
 	var (
@@ -15,6 +17,11 @@ func main() {
 		h func() string
 		l = 200.234
 	)
+
+	m := "19"
+	n := "190"
+	m, n, q := "a", "b", 10  //q 为新变量，可以初始化定义模式赋值
+	m, _, r := "c", "d", 100 //匿名变量
 	fmt.Printf("%T,%v \n", a, a)
 	fmt.Printf("%T,%q,%v \n", b, b, b)
 	fmt.Printf("%T,%v \n", c, c)
@@ -24,5 +31,17 @@ func main() {
 	fmt.Printf("%T,%v \n", g, g)
 	fmt.Printf("%T,%v \n", h, h)
 	fmt.Printf("%T,%v \n", l, l)
+	fmt.Printf("%T,%v \n", m, m)
+	fmt.Printf("%T,%v \n", n, n)
+	fmt.Printf("%T,%v \n", q, q)
+	fmt.Printf("%T,%v \n", r, r)
 	fmt.Println(a, b, c, d, e, f, g, h, l)
+
+	// 变量直接交换，程序员期待已久的语法
+	x := 10
+	y := 20
+	z := 30
+	fmt.Println(x, "  ", y, "  ", z)
+	x, y, z = y, z, x
+	fmt.Println(x, "  ", y, "  ", z)
 }
